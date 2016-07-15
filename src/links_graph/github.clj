fa3174d49d070ea14a59d35ced6a1a9acc5a9b22
Anton Erholt <anton.erholt@gmail.com>
2016-07-15 12:48:20 +0200

    Kept working on github integration

--- src/links_graph/github.clj
+++ src/links_graph/github.clj
@@ -1,8 +1,9 @@
 (ns links-graph.github
   (:require [tentacles.users]
             [tentacles.repos]
+            [tentacles.events]
+            [tentacles.data]
             [clojure.data.json :as json]))
-
 
 (def auth-str
   (clojure.string/join
@@ -18,10 +19,42 @@
   {:user-agent user-agent
    :auth       auth-str})
 
+(def gh-user "antonaut")
+(def gh-repo "working-process")
+
+
 (defn my-emails
   []
   (tentacles.users/emails default-options))
 
 (defn my-repos
   []
-  (tentacles.repos/user-repos "antonaut" default-options))
+  (tentacles.repos/user-repos gh-user default-options))
+
+
+
+;;(tentacles.events/repo-events "antonaut" "working-process")
+
+(def master-commit-obj (-> (tentacles.data/references gh-user gh-repo default-options)
+                       first
+                       :object))
+
+
+(defn get-commit [sha]
+  (tentacles.data/commit gh-user gh-repo sha default-options))
+
+(defn parent-commits [commit]
+  (loop [commit commit
+         cnt 10]
+    (when (and (not= 404 (:status commit))
+               (> 0 cnt))
+      (do
+        (println commit)
+        (recur (get-commit (:sha (first (:parents commit)))) (dec cnt))))))
+
+(parent-commits (get-commit (:sha master-commit-obj)))
+
+(tentacles.data/tree gh-user gh-repo "3ead18a654bfdb80187393e266c2193c7929c7e6" default-options)
+
+
+;; (map gh-repo->ardoq my-repos)
